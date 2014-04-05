1ba30f3822925c10614180e199b1025c2c75155a
Stella <sctkyuna@sctk.(none)>
2014-04-05 13:54:03 -0400

    database

--- schema.sql
+++ schema.sql
@@ -1,7 +1,18 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title text not null,
-  text text not null
+drop table if exists event;
+create table event (
+  eventID integer primary key autoincrement,
+  name text not null,
+  details text not null,
+);
+
+drop table if exists trans;
+create table trans (
+  transID integer autoincrement,
+  eventID integer,
+  payerUID integer not null,
+  recipUID integer not null,
+  amount real,
+  PRIMARY KEY(transid, eventid),
+  FOREIGN KEY(eventID) REFERENCES event(eventID)
 );
 
