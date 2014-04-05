ebb2babb316c24dc3178e2a59a08c30d3c5fd391
Max Lee <mlee177@jhu.edu>
2014-04-05 14:59:35 -0400

    stuff

--- schema.sql
+++ schema.sql
@@ -1,18 +1,38 @@
 drop table if exists event;
 create table event (
-  eventID integer primary key autoincrement,
+  ID integer primary key autoincrement,
   name text not null,
   details text not null,
 );
 
 drop table if exists trans;
 create table trans (
-  transID integer autoincrement,
+  ID integer primary key autoincrement,
   eventID integer,
   payerUID integer not null,
   recipUID integer not null,
   amount real,
-  PRIMARY KEY(transid, eventid),
-  FOREIGN KEY(eventID) REFERENCES event(eventID)
+  FOREIGN KEY(eventID) REFERENCES event(ID),
+  FOREIGN KEY(payerUID) REFERENCES user(ID),
+  FOREIGN KEY(recipUID) REFERENCES user(ID)
 );
+
+drop table if exists user;
+create table user (
+	ID integer primary key autoincrement,
+	firstname text not null,
+	lastname text not null,
+	email text,
+	phone text
+);
+
+drop table if exists participants
+create table participants (
+	userID integer,
+	eventID integer,
+	PRIMARY KEY (userID, eventID),
+	FOREIGN KEY (eventID) REFERENCES event(ID),
+	FOREIGN KEY (userID) REFERENCES user(ID)
+);
+
 
